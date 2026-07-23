c382dafaa7707f8e64286f868354d7bdf5d32306
NguyenQuy <quynguyen34255@gmail.com>
2026-07-23 09:23:39 +0700

    tao blazor

--- QuanLyKhachSan_fix/MauiProgram.cs
+++ QuanLyKhachSan_fix/MauiProgram.cs
@@ -33,12 +33,12 @@ namespace QuanLyKhachSan_fix
             builder.Services.AddScoped<ICustomerAccountService, CustomerAccountService>();
             builder.Services.AddScoped<IReportService, ReportService>();
 
-            // TODO: 2 thanh vien con lai them dong dang ky Service cua minh vao day
+            // TODO: 1 thanh vien con lai them dong dang ky Service cua minh vao day
             // builder.Services.AddScoped<IBookingService, BookingService>();       // Thanh vien 1
             // builder.Services.AddScoped<IPaymentService, PaymentService>();       // Thanh vien 1
             // builder.Services.AddScoped<IBookingEditRequestService, BookingEditRequestService>(); // Thanh vien 1
-            // builder.Services.AddScoped<ICheckInOutService, CheckInOutService>(); // Thanh vien 2
-            // builder.Services.AddScoped<IInvoiceService, InvoiceService>();       // Thanh vien 2
+            builder.Services.AddScoped<ICheckInOutService, CheckInOutService>(); // Thanh vien 2
+            builder.Services.AddScoped<IInvoiceService, InvoiceService>();       // Thanh vien 2
 
             // ---- Hết phần thêm ----
 
